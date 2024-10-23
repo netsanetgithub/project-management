7de1e0acc60a533d5198c1cfe052803c5f3be7d6
Netsanet Adera  Jima <NetsanetAdera@cbe.com.et>
2024-10-23 14:28:32 +0300

    v1

--- back_end/index.js
+++ back_end/index.js
@@ -1,29 +1,15 @@
 require('dotenv').config();
 const express=require('express');
+const project=require('./project.json');
 const port=process.env.PORT;
 
 const app=express();
-const project=[
-    {
-    "id":"1",
-    "name":"sdwan",
-    "start_date":"1-1-2023",
-    "end_date":"12-2-2023",
-    },
-    {
-        "id":"2",
-        "name":"pcidss",
-        "start_date":"1-1-2023",
-        "end_date":"12-2-2023",
-        },
-
-    
-       ];
 
 app.get('/projects',(req,res)=>{
       
       res.send(project)
 });
+
 
 app.get('/project',(req,res)=>{
     const id=req.query.id;
